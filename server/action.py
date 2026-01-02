f9940582dc6ce733afd36ec48de315c41601c4e5
Faeze-ghandi <faezeghandi91@gmail.com>
2026-01-02 23:43:31 +0330

    action-step0

---
+++ server/action.py
@@ -0,0 +1,23 @@
+from enum import Enum, auto
+
+class ActionType(Enum):
+    Move = auto()
+    BuyProperty = auto()
+    PayRent = auto()
+    Trade = auto()
+    Build = auto()
+    Mortgage = auto()
+    CardEffect = auto()
+
+
+class Action:
+    ID=0
+    def __init__(self, id):
+        Action.ID +=1
+        self.action_id = Action.ID
+        self.action_type = ActionType.Move
+        self.actor_id = id
+        self.affected_entities= []
+        self.previous_state = None
+        self.new_state = None
+
